3702889bd0140e3ce2e0395d7f56afc5b35ea4ac
Alex Sova <foxeyes.ml@gmail.com>
2022-06-22 08:25:49 +0300

    feat: jamImport + colorCode

--- iso/index.js
+++ iso/index.js
@@ -3,3 +3,5 @@ export { htmlMin } from './htmlMin.js';
 export { cssMin } from './cssMin.js';
 export { buildHtml } from './buildHtml.js';
 export { fromUrl } from './fromUrl.js';
+export { colorCode } from './colorCode.js';
+export * as jamImport from './jamImport.js';
